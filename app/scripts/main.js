64bf252655031bec267072a24e266a83a5347204
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-07 15:07:19 -0500

    interactin api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('The Iron Yard Rocks');
+// var u = "https://www.googleapis.com/books/v1/volumes?q=fiction&callback=handleResponse";
+
+
+
+
+
+//
+// var bookTemplate = $('#starredApi').html();
+// var render = _.template(starredTemplate);
+// var url = "https://www.googleapis.com/books/v1/volumes?q=fiction&callback=handleResponse";
+//
+// $.getJSON(url).done(
+//   function (user) {
+//     user.forEach( function(x) {
+//     $('.count2').append( showStarred(x));
+//   });
+//
+// });
