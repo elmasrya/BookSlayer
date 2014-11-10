a9fc7b8022ae44b5902c3b952fa67d93202c12b8
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-09 23:49:34 -0500

    search fixed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,48 @@
-// var u = "https://www.googleapis.com/books/v1/volumes?q=fiction&callback=handleResponse";
+
+$('.search').on('click', function(){
+      event.preventDefault();
+      var inputVal=$('.whatever').val();
+      console.log(inputVal);
+
+
+  var googleApi="https://www.googleapis.com/books/v1/volumes?q="+inputVal+"&callback=handleResponse";
 
 
 
 
 
-//
-// var bookTemplate = $('#starredApi').html();
-// var render = _.template(starredTemplate);
-// var url = "https://www.googleapis.com/books/v1/volumes?q=fiction&callback=handleResponse";
-//
-// $.getJSON(url).done(
-//   function (user) {
-//     user.forEach( function(x) {
-//     $('.count2').append( showStarred(x));
-//   });
-//
-// });
+
+
+
+  // Send item to server
+  $.ajax({
+    type: 'GET',
+    url: googleApi
+
+
+  });
+
+});
+
+    function handleResponse(response) {
+      for (var i = 0; i < response.items.length; i++) {
+        var item = response.items[i];
+        // in production code, item.text should have the HTML entities escaped.
+
+        if(item.volumeInfo.pageCount===undefined) {
+
+
+        $('#content').append("<br>" + "Book Title: " + item.volumeInfo.title + "<br>" + "Pages: None " + "<br>");
+
+
+        } else{
+          console.log("giants");
+        $('#content').append("<br>" + "Book Title: " + item.volumeInfo.title + "<br>" + "Pages: " + item.volumeInfo.pageCount + "<br>");
+
+
+        }
+
+      }
+
+
+    }/*end of handleResponse*/
