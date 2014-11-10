139b85b74ef11f9a3ef27a44bba9dc23d3598d3d
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-10 14:07:01 -0500

    books

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,63 @@
+(function () {
+
+  App.Models.Book = Backbone.Model.extend({
+
+    idAttribute: '_id',
+
+    defaults: {
+      Title: '',
+      Page: '',
+    },
+
+    initialize: function () {
+      // var t = this.get('');
+      //console.log(t + " has been added");
+    }
+
+  });
+
+}());
 
 
 
 
+// (function () {
+//
+//   App.Views.AddBook = Backbone.View.extend({
+//
+//     events: {
+//       'submit #addCoffee' : 'addCoffee'
+//     },
+//
+//     initialize: function () {
+//       this.render();
+//
+//       $('#coffeeList').html(this.$el);
+//     },
+//
+//     render: function () {
+//       this.$el.html($('#addTemp').html());
+//     },
+//
+//     addCoffee: function (e) {
+//       e.preventDefault();
+//
+//       var c = new App.Models.Coffee({
+//         name: $('#coffee_name').val(),
+//         brand: $('#coffee_brand').val()
+//       });
+//
+//       App.coffees.add(c).save(null, {
+//         success: function () {
+//           App.router.navigate('', { trigger: true });
+//         }
+//       });
+//
+//     }
+//
+//   });
+//
+// }());
 
 
 
@@ -38,12 +94,12 @@ $('.search').on('click', function(){
         if(item.volumeInfo.pageCount===undefined) {
 
 
-        $('#content').append("<br>" + "Book Title: " + item.volumeInfo.title + "<br>" + "Pages: None " + "<br>");
+        $('#content').append("<br>" + "<button>" + "Book Title: " + item.volumeInfo.title + "<br>" + "Pages: None " + "</button>" + "<br>");
 
 
         } else{
           console.log("giants");
-        $('#content').append("<br>" + "Book Title: " + item.volumeInfo.title + "<br>" + "Pages: " + item.volumeInfo.pageCount + "<br>");
+        $('#content').append("<br>"+ "<button>" + "Book Title: " + item.volumeInfo.title + "<br>" + "Pages: " + item.volumeInfo.pageCount + "</button>" + "<br>");
 
 
         }
