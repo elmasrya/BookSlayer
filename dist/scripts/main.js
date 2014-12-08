30a2bb8e67963b2179adba29c7309d24c2c2c64b
Andrew El-Masry <andrewelmasry@gmail.com>
2014-12-08 13:46:35 -0500

    testing

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -51,7 +51,7 @@
       'search'            : 'search',
       'addBook'           : 'addBook',
       'test'              : 'test',
-      'single:objectId'   : 'single'
+      'single/:objectId'   : 'single'
     },
 
     home: function () {
@@ -109,11 +109,57 @@
     },
 
     single: function (objectId) {
-      $('#tools').empty();
       var singleBook = App.books.get(objectId);
-      new App.Views.SingleBook({book: singleBook, collection: App.books});
+      new App.Views.SingleBook({book: singleBook, collection: App.books, user: App.user});
       new App.Views.Footer();
     }
+
+
+
+  });
+
+}());
+
+(function () {
+
+
+  App.Views.NavBar = Parse.View.extend({
+
+
+    el                : '#top',
+
+    template          : _.template($('#navBarTemp').html()),
+
+    events: {
+      'click .logout' : 'logout'
+    }, // end of events
+
+
+    initialize: function (options) {
+
+      this.options=options;
+      this.render();
+
+
+    },
+
+    render: function () {
+
+      this.$el.html(this.template);
+    },
+
+
+    logout: function(e){
+
+      var current = this.options;
+      Parse.User.logOut();
+      App.user=null;
+      App.router.navigate('', { trigger: true });
+      new App.Views.Home();
+      location.reload();
+    }
+
+
 
 
 
@@ -139,16 +185,15 @@ events: {
     initialize: function (options) {
       this.options=options;
       this.render();
-      console.log(options);
       $("#middle").html(this.$el);
     },
 
     render: function () {
       var self = this;
 
-      // $("#middle").empty();
-      //
-      // this.$el.empty();
+      $("#middle").empty();
+
+      this.$el.empty();
 
       this.$el.html(this.template(this.options.book.toJSON()));
 
@@ -199,10 +244,8 @@ events: {
 
   App.Views.AddBook = Parse.View.extend({
 
-    el             : '#middle',
-
-    tagName: 'ul',
-    className: 'bookItem',
+    tagName: 'form',
+    className: 'createForm',
 
 
     template        : _.template($('#addBookTemp').html()),
@@ -214,10 +257,14 @@ events: {
 
     initialize   : function () {
       this.render();
+      $("#middle").html(this.$el);
+
     },
 
     render  : function () {
-      this.$el.html(this.template);
+      $("#middle").empty();
+
+      this.$el.html(this.template());
 
     },
 
@@ -419,7 +466,6 @@ events: {
 
   App.Views.NavBar = Parse.View.extend({
 
-    tagName: 'nav',
 
     el                : '#top',
 
@@ -444,12 +490,14 @@ events: {
     },
 
 
-    logout: function(){
+    logout: function(e){
 
       var current = this.options;
       Parse.User.logOut();
       App.user=null;
+      App.router.navigate('', { trigger: true });
       new App.Views.Home();
+      location.reload();
     }
 
 
@@ -490,16 +538,20 @@ events: {
 
     goToAccount : function () {
       // Log User into account
+
       var username = $('.usernameVal').val();
       var password = $('.passwordVal').val();
 
       Parse.User.logIn(username, password, {
         success: function (user) {
           App.user=user;
+          location.reload();
           console.log('Login successful');
             App.router.navigate('', {trigger: true});
+
         },
         error: function (user, error) {
+          location.reload();
           alert('Invalid user login');
         }
       }); // end of logIn
@@ -617,46 +669,96 @@ events: {
 (function () {
 
 
-  App.Views.NavBar = Parse.View.extend({
+  App.Views.Footer = Parse.View.extend({
 
-    tagName: 'nav',
 
-    el                : '#top',
+    el                : '#bottom',
 
-    template          : _.template($('#navBarTemp').html()),
+    template          : _.template($('#footerTemp').html()),
 
     events: {
-      'click .logout' : 'logout'
+
+
     }, // end of events
 
 
-    initialize: function (options) {
-
-      this.options=options;
+    initialize: function () {
       this.render();
-
 
     },
 
     render: function () {
 
       this.$el.html(this.template);
+
     },
-
-
-    logout: function(){
-
-      var current = this.options;
-      Parse.User.logOut();
-      App.user=null;
-      new App.Views.Home();
-    }
-
-
 
 
 
   });
+
+}());
+
+Parse.initialize("MCSNBPaOJowTLp0LZqcG2hPRLKsPlTqHeKT3CK3P","JRsBK5g0aam1H89lxIcZIqRTSllbuiASaNIkz9G7");
+
+(function() {
+
+  App.books = new App.Collections.Books();
+
+  App.user = Parse.User.current();
+
+  new App.Views.NavBar({user: App.user});
+
+  App.books.fetch().done(function () {
+      App.router = new App.Routers.AppRouter();
+      Parse.history.start();
+  });
+
+  App.handleResponse = function (response) {
+    console.log("working");
+    for (var i = 0; i < response.items.length; i++) {
+      var item = response.items[i];
+      // in production code, item.text should have the HTML entities escaped.
+
+      if(item.volumeInfo.pageCount===undefined) {
+
+
+      $('.searchResults').append("<br>" + "<button class='choices' id='"+ i + "'> + </button>" +
+        "<span class='book' id='" + i +"'>" +"Book Title: " + item.volumeInfo.title + "<br>" + "Pages: Not Available " + "<br>" + "<br>"
+        + "</span>");
+
+
+      } else{
+
+      $('.searchResults').append("<br>" + "<button class='choices' id='"+ i + "'> + </button>" +
+        "<span class='book' id='" +  i +"'>" +"Book Title: " + item.volumeInfo.title + "<br></br>" + "<a href="+ item.volumeInfo.previewLink + ">Preview</a>" +"<img src="+ item.volumeInfo.imageLinks.thumbnail + "/>" +"<br>" + "Pages: " + item.volumeInfo.pageCount
+       + "<br>" + "<br>" + "</span>");
+
+
+      }
+
+
+
+
+    }//End of loop
+
+
+      $('.choices').on('click', function(){
+
+        var elemID = this.id;
+        console.log(elemID);
+
+        var bookInfo = $('.book[id=' + elemID + ']').text();
+        // var =item.volumeInfo.title;
+        // var b=item.volumeInfo.pageCount;
+
+
+      $('.addBookSec').append("<br>" + bookInfo + "<br>");
+      var exit=$('.addBookSec').val();
+
+      });
+
+  }
 
 }());
 
@@ -697,113 +799,23 @@ events: {
       var inputVal=$('.searchBar').val();
       console.log(inputVal);
 
-      var googleApi="https://www.googleapis.com/books/v1/volumes?q="+inputVal+"&callback=handleResponse";
+      var googleApi="https://www.googleapis.com/books/v1/volumes?q="+inputVal+"&callback=App.handleResponse";
       $.ajax({
         type: 'GET',
         url: googleApi
       });
 
-      function handleResponse(response) {
 
-        for (var i = 0; i < response.items.length; i++) {
-          var item = response.items[i];
-          // in production code, item.text should have the HTML entities escaped.
-
-          if(item.volumeInfo.pageCount===undefined) {
-
-
-          $('.searchResults').append("<br>" + "<button class='choices' id='"+ i + "'> + </button>" +
-            "<span class='book' id='" + i +"'>" +"Book Title: " + item.volumeInfo.title + "<br>" + "Pages: Not Available " + "<br>" + "<br>"
-            + "</span>");
-
-
-          } else{
-
-          $('.searchResults').append("<br>" + "<button class='choices' id='"+ i + "'> + </button>" +
-            "<span class='book' id='" +  i +"'>" +"Book Title: " + item.volumeInfo.title + "<br></br>" + "<a href="+ item.volumeInfo.previewLink + ">Preview</a>" +"<img src="+ item.volumeInfo.imageLinks.thumbnail + "/>" +"<br>" + "Pages: " + item.volumeInfo.pageCount
-           + "<br>" + "<br>" + "</span>");
-
-
-          }
-
-
-
-
-        }//End of loop
-
-
-          $('.choices').on('click', function(){
-
-            var elemID = this.id;
-            console.log(elemID);
-
-            var bookInfo = $('.book[id=' + elemID + ']').text();
-            // var =item.volumeInfo.title;
-            // var b=item.volumeInfo.pageCount;
-
-
-          $('.addBookSec').append("<br>" + bookInfo + "<br>");
-          var exit=$('.addBookSec').val();
-
-          });
-
-      }
-
-
-    }
-
-
-
-
-  });
-
-}());
-
-(function () {
-
-
-  App.Views.Footer = Parse.View.extend({
-
-
-    el                : '#bottom',
-
-    template          : _.template($('#footerTemp').html()),
-
-    events: {
-
-
-    }, // end of events
-
-
-    initialize: function () {
-      this.render();
 
     },
 
-    render: function () {
-
-      this.$el.html(this.template);
+    delete : function () {
 
     },
 
 
 
-  });
 
-}());
-
-Parse.initialize("MCSNBPaOJowTLp0LZqcG2hPRLKsPlTqHeKT3CK3P","JRsBK5g0aam1H89lxIcZIqRTSllbuiASaNIkz9G7");
-
-(function() {
-  App.books = new App.Collections.Books();
-
-  App.user = Parse.User.current();
-
-  new App.Views.NavBar({user: App.user});
-
-  App.books.fetch().done(function () {
-      App.router = new App.Routers.AppRouter();
-      Parse.history.start();
   });
 
 }());
